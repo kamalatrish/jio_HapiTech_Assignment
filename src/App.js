4ba206d12942393d2ffc530c2920b0ce346ea46f
vn51zks <vn51zks@homeoffice.wal-mart.com>
2021-12-06 22:09:23 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+
+import { BsTrash, BsStarFill, BsStar, BsSearch } from "react-icons/bs";
 import * as apiFunc from '../src/actions/action'
 
 function App() {
   const [name, updateName] = React.useState("")
   const [dataList, updateList] = React.useState([]); // type array for iteration
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     let getList = apiFunc.getData();
     updateList(getList);
-  },[])
+  }, [])
 
 
   let submitName = () => {
-    console.log("heuhiohi");
-    let updateResponse = apiFunc.updateList(dataList,name);
-    updateList(updateResponse);
-    updateName("");
+    if (name == 0) {
+      alert("Enter name please !!")
+    }
+    else {
+      let updateResponse = apiFunc.updateList(dataList, name);
+      updateList(updateResponse);
+      updateName("");
+    }
   }
 
   let deleteFriend = (index) => {
-    console.log("deleteFunction",index);
+    let deleteList = [...dataList];
+    deleteList.splice(index, 1)
+    updateList(deleteList);
+  }
+
+  let favFriend = (index) => {
+    let favList = [...dataList];
+    favList[index].isStarred = !favList[index].isStarred;
+    updateList(favList);
   }
 
   return (
     <div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <div >
-        <div className="colorGray">Friend List</div>
-        <div>
-          <input type="text" value={name} onChange={(e) => updateName(e.target.value)} placeholder="Enter Your Friend Name" />
-          <button onClick={() => submitName()}>Submit</button>
+        <div className="displayFlex">
+          <div className="title divide">Friend List</div>
+          <div className="searchName title">
+            <input type="text" value={name} onChange={(e) => updateName(e.target.value)} placeholder="search Friend Name" />
+            <BsSearch />
+          </div>
         </div>
         <div>
           {dataList.map((item, index) => {
@@ -52,15 +57,22 @@ function App() {
               <div className="displayFlex" key={index}>
                 <div className="divide">
                   <div>{item.name}</div>
-                  <div>is your friend</div>
+                  <div className="supportText">is your friend</div>
                 </div>
                 <div className="divide">
-                  <button>Star button</button>
-                  <button onClick={()=>deleteFriend(index)}>Delete button</button>
+                  {item.isStarred ?
+                    <BsStarFill className="starFillIcon" onClick={() => favFriend(index)} /> :
+                    <BsStar className="starIcon" onClick={() => favFriend(index)} />
+                  }
+                  <BsTrash onClick={() => deleteFriend(index)} className="trashIcon" />
                 </div>
               </div>
             )
           })}
+        </div>
+        <div className="displayFlex addFriend">
+          <input className="divide" type="text" value={name} onChange={(e) => updateName(e.target.value)} placeholder="Enter Your Friend Name" />
+          <button className="submitBtn" onClick={() => submitName()}>Submit</button>
         </div>
       </div>
     </div>
